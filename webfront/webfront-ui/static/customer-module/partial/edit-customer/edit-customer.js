3929a4284eb143b35d43a70f5f56f06d21f44651
unknown <sambhav.jain@HC14-185.hcentive.com>
2016-05-30 17:28:33 +0530

    ui hell

--- webfront/webfront-ui/static/customer-module/partial/edit-customer/edit-customer.js
+++ webfront/webfront-ui/static/customer-module/partial/edit-customer/edit-customer.js
@@ -1,4 +1,20 @@
-angular.module('customerModule').controller('EditCustomerCtrl',
-    function($scope, customer) {
-        console.log(customer);
-    });
+angular
+    .module('customerModule')
+    .controller('EditCustomerCtrl',
+        function($scope, customer, customerService) {
+            console.log(customer);
+            if (customer.id) {
+                console.debug("Edit flow");
+            } else {
+                console.debug("New Customer flow.");
+            }
+
+            $scope.saveCustomer = function() {
+                if ($scope.customerForm.$valid) {
+                    $scope.customer = customerService.save($scope.customer);
+                    console.debug($scope.customer);
+                } else {
+                    console.error($scope.customerForm.$error);
+                }
+            };
+        });
--- webfront/webfront-ui/static/customer-module/service/customer-service.js
+++ webfront/webfront-ui/static/customer-module/service/customer-service.js
@@ -4,7 +4,15 @@ angular.module('customerModule').factory('customerService',
         return {
             find: function(id) {
                 console.debug("Getting customer with id " + id);
-                return { name: "Kumar Sambhav" };
+                return { fullName: "Kumar Sambhav" };
+            },
+            save: function(customer) {
+                console.debug('Saving Customer details');
+                customer.id = 10;
+                return customer;
+            },
+            update: function(customer) {
+                console.debug("Updating customer details.");
             }
         };
     });
